b5cde6a3881c24f0b2ca4764f60f43b0a60227e9
keita shinohara <keita.shinohara@gmail.com>
2018-08-07 20:56:47 +0900

    add ReLU function

--- activation_function.py
+++ activation_function.py
@@ -10,9 +10,14 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+def relu(x):
+    return np.maximum(0, x)
+
+
 x = np.arange(-5.0, 5.0, 0.1)
 y1 = step_function(x)
 y2 = sigmoid(x)
-plt.plot(x, y1, x, y2)
-plt.ylim(-0.1, 1.1)
+y3 = relu(x)
+plt.plot(x, y1, x, y2, x, y3)
+plt.ylim(-0.1, 5.1)
 plt.show()
