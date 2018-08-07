b67cc160f985f989a7243983a82a89783d5b29a8
keita shinohara <keita.shinohara@gmail.com>
2018-08-07 20:45:13 +0900

    add sigmoid function

--- activation_function.py
+++ activation_function.py
@@ -6,8 +6,13 @@ def step_function(x):
     return np.array(x > 0, dtype=np.int)
 
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
 x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
-plt.plot(x, y)
+y1 = step_function(x)
+y2 = sigmoid(x)
+plt.plot(x, y1, x, y2)
 plt.ylim(-0.1, 1.1)
 plt.show()
