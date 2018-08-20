53665ee1b72e1c3d5b6a0753ddff97b0430c8cbf
keita shinohara <keita.shinohara@gmail.com>
2018-08-20 22:40:45 +0900

    add comments

--- gradient/gradient.py
+++ gradient/gradient.py
@@ -37,6 +37,8 @@ def numerical_gradient(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
 
+    # イテレータを生成している模様。
+    # 詳しい解説はそのうち学ぼう。
     it = np.nditer(x, flags=["multi_index"], op_flags=["readwrite"])
     while not it.finished:
         idx = it.multi_index
